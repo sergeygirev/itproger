673ff5e9e7189befe95ac55979648dd97db4397c
Sergey Girev <sergeygirev@MacBook-Air.local>
2025-08-25 22:13:36 +0200

    Initial commit

---
+++ lesson3.py
@@ -0,0 +1,14 @@
+# переменные
+# number = 5 # int
+# # print('Result:', number)
+# del number
+# number = 7 #int
+# boolean = False #bool
+# digit = -4.556586 # float
+# # print(number + digit)
+# word = 'Result:' #string
+# # print(word + str(digit))
+# digit = '-4.552525' # string
+# # print(word + digit)
+# strum = '5' #string
+# print(word + str(number + int(strum)))
---
+++ lesson4.py
@@ -0,0 +1,16 @@
+# numb1 = int(input('Введите первое число:'))
+# # string
+# numb2 = int(input('Введите первое число:'))
+# numb1 += 5
+# print('Result:', numb1 + numb2)
+# print('Result:', numb1 - numb2)
+# print('Result:', numb1 / numb2)
+# print('Result:', numb1 * numb2)
+# print('Result:', numb1 ** numb2)
+# print('Result:', numb1 // numb2)
+# word = 'Hi'
+# print(word * 2)
+# homework from chatgpt
+# name = input('Ваше имя:')
+# age = input('Ваш возраст:')
+# print('Привет,',name,'!''Тебе',age,'лет.')
